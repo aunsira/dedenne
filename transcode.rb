7b8ec090893c198b6ab0f0edd20fa1ee5cb168f6
aun <aun.sira@gmail.com>
2016-09-07 17:10:29 +0700

    Prove some concept out

--- transcode.rb
+++ transcode.rb
@@ -8,11 +8,17 @@ puts "Video codec :: " + video.video_codec
 puts "Resolution :: " +  video.resolution
 
 options = {
-  custom: %w(-c:v h264 -hls_flags delete_segments -hls_key_info_file file.keyinfo)
+custom: %w(-s 1920x1080 -c:v libx264 -hls_flags delete_segments -hls_key_info_file file.keyinfo)
 }
 
 transcoder_options = { validate: false }
 video.transcode("/Users/aun/tmp/transcode/files/transcoded.m3u8", options, transcoder_options) do |progress|
   puts progress
 end
+
+video = FFMPEG::Movie.new("/Users/aun/tmp/transcode/files/transcoded.m3u8")
+
+puts "Audio stream :: " + video.audio_stream.to_s
+puts "Video codec :: " + video.video_codec.to_s
+puts "Resolution :: " +  video.resolution.to_s
 
