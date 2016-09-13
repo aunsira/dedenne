f45488cd81e7fdb0d0359db818bf1281240e1898
aun <aun.sira@gmail.com>
2016-09-13 15:24:49 +0700

    Transcode with 3 bit rates

--- transcode.rb
+++ transcode.rb
@@ -1,6 +1,9 @@
 require 'streamio-ffmpeg'
 require 'fileutils'
 
+FileUtils.mkdir('./files') unless File.exists? './files'
+FileUtils.cp './file.key', './files/file.key'
+
 video = FFMPEG::Movie.new(ARGV[0])
 
 puts video.valid?
@@ -8,20 +11,31 @@ puts "Audio stream :: " + video.audio_stream
 puts "Video codec :: " + video.video_codec
 puts "Resolution :: " +  video.resolution
 
-options = {
-  custom: %w(-b:v 300k -s 1920x1080 -c:v h264 -hls_time 1 -hls_list_size 0 -hls_key_info_file file.keyinfo -f hls)
+bitrates = {
+  480 => '900',
+  720 => '1800',
+  1080 => '3000'
 }
 
-transcoder_options = { validate: false }
-video.transcode(ARGV[1], options, transcoder_options) do |progress|
-  puts progress
+bitrates.each do |quality, bitrate|
+  segment_file = "./files/out_#{quality}"
+  output_file = segment_file + ".m3u8"
+
+  options = {
+    custom: %W( -b:v #{bitrate}k
+                -s 1920x1080
+                -c:v h264
+                -hls_time 1
+                -hls_list_size 0
+                -hls_segment_filename #{segment_file}_%03d.ts
+                -hls_key_info_file file.keyinfo
+                -r 30
+                -f hls )
+  }
+
+  transcoder_options = { validate: false }
+  video.transcode(output_file, options, transcoder_options) do |progress|
+    puts progress
+  end
 end
-
-FileUtils.cp './file.key', './files/file.key'
-
-video = FFMPEG::Movie.new(ARGV[1])
-
-puts "Audio stream :: " + video.audio_stream.to_s
-puts "Video codec :: " + video.video_codec.to_s
-puts "Resolution :: " +  video.resolution.to_s
 
