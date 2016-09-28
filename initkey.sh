96949049bd59537563d948d1bf5c6b2e42e48473
aun <aun.sira@gmail.com>
2016-09-28 10:48:23 +0700

    Fix key path

--- initkey.sh
+++ initkey.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-BASE_URL=${1:-'.'}
+# BASE_URL=${1:-'.'}
 qualities="480 720 1080"
 _p="p_"
 for quality in $qualities
 do
   openssl rand 16 > hls_$quality$_p.key
-  echo $BASE_URL/hls_$quality$_p.key > hls_$quality$_p.keyinfo
+  echo hls_$quality$_p.key > hls_$quality$_p.keyinfo
   echo hls_$quality$_p.key >> hls_$quality$_p.keyinfo
   echo $(openssl rand -hex 16) >> hls_$quality$_p.keyinfo
 done
