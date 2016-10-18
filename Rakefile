ed7df866b508f044217afeb059f5ed949a47dd7e
aun <aun.sira@gmail.com>
2016-10-18 14:54:12 +0700

    Add ability to connect redis from other container

--- Rakefile
+++ Rakefile
@@ -1,2 +1,5 @@
-require 'resque/tasks'
 require_relative 'app'
+require_relative 'config/resque'
+require 'resque'
+Resque.redis = Config.redis_url || Config.local_redis
+require 'resque/tasks'
--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'resque'
 require_relative 'lib/dedenne'
 require_relative 'lib/dedenne/transcoder_queue'
+require_relative 'config/resque'
 
 set :bind, '0.0.0.0'
 
@@ -9,5 +10,7 @@ get '/transcode/course/:course_id/chapter/:chapter_id' do
   course_id     = params['course_id']
   chapter_id    = params['chapter_id']
   video_version = "-#{params['version']}" || ""
+
+  Resque.redis = Config.redis_url || Config.local_redis
   Resque.enqueue(TranscoderQueue, course_id, chapter_id, video_version)
 end
