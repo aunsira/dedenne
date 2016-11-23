5a6230101a98086684ccea042c4aeeb25394a9d8
aun <aun.sira@gmail.com>
2016-11-23 15:56:53 +0700

    WE TEST!

--- spec/dedenne/ffmpeg_spec.rb
+++ spec/dedenne/ffmpeg_spec.rb
@@ -1,15 +1,17 @@
 require 'spec_helper'
+require 'streamio-ffmpeg'
 require 'dedenne/ffmpeg'
+require 'fileutils'
 
 
 describe Dedenne::FFMPEGHLS do
-  describe 'initialize ffmpeg' do
-    it 'testing' do
-      course_id = 1
-      chapter_id = 2
-      video_version = 1
-      foo = Dedenne::FFMPEGHLS.new(course_id, chapter_id, video_version)
-      expect(foo).to eq nil
+  describe 'initialize' do
+    it 'Should find a video file' do
+      course_id = "1"
+      chapter_id = "2"
+      video_version = "-1"
+      allow(FFMPEG::Movie).to receive(:new).and_return("/Users/aun/video/#{course_id}/#{chapter_id}#{video_version}.mp4")
+      Dedenne::FFMPEGHLS.new(course_id, chapter_id, video_version)
     end
   end
 end
