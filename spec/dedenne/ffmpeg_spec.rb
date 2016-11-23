bae9ad56f9377488ab232d5380b2e485d9bf2225
aun <aun.sira@gmail.com>
2016-11-23 16:32:42 +0700

    Add test for hashing

--- spec/dedenne/ffmpeg_spec.rb
+++ spec/dedenne/ffmpeg_spec.rb
@@ -5,15 +5,24 @@ require 'fileutils'
 
 
 describe Dedenne::FFMPEGHLS do
+  course_id = "1"
+  chapter_id = "2"
+  video_version = "-1"
+
   describe 'initialize' do
-    course_id = "1"
-    chapter_id = "2"
-    video_version = "-1"
     let(:video) { FFMPEG::Movie.new("/Users/aun/video/#{course_id}/#{chapter_id}#{video_version}.mp4") }
 
     it 'Should find a video file' do
       allow(FFMPEG::Movie).to receive(:new).and_return(video)
       Dedenne::FFMPEGHLS.new(course_id, chapter_id, video_version)
+    end
+  end
+
+  describe 'hashing' do
+    it 'should hash correctly' do
+      ffmpeg = Dedenne::FFMPEGHLS.new(course_id, chapter_id, video_version)
+      hash_code = ffmpeg.hash
+      expect(hash_code).to eq("36155ffb5f8a6e48dd040c384ccf28d24e0746c4")
     end
   end
 end
