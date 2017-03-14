7feca84b54666d5c4dcb38e3fff4149bbae0f14a
aun <aun.sira@gmail.com>
2017-03-14 18:28:48 +0700

    Add REDIS_PASSWORD to compose file

--- Rakefile
+++ Rakefile
@@ -2,5 +2,5 @@ require_relative 'app'
 require_relative 'config/resque'
 require 'resque'
 require 'redis'
-Resque.redis = Redis.new(url: Config.redis_url) || Config.local_redis
+Resque.redis = Redis.new(url: Config.redis_url)
 require 'resque/tasks'
--- config/resque.rb
+++ config/resque.rb
@@ -1,11 +1,5 @@
 class Config
   def self.redis_url
-    if ENV['REDIS_PORT_6379_TCP_ADDR'] && ENV['REDIS_PORT_6379_TCP_PORT'] && ENV['REDIS_PASSWORD']
-      return "redis://:#{ENV['REDIS_PASSWORD']}@#{ENV['REDIS_PORT_6379_TCP_ADDR']}:#{ENV['REDIS_PORT_6379_TCP_PORT']}"
-    end
-  end
-
-  def self.local_redis
-    'localhost:6379'
+    "redis://:#{ENV['REDIS_PASSWORD']}@#{ENV['REDIS_PORT_6379_TCP_ADDR']}:#{ENV['REDIS_PORT_6379_TCP_PORT']}"
   end
 end
